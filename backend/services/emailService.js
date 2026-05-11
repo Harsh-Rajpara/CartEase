94667e3a1c740ede50c619f412d1aae512f558ba
Harsh Rajpara <hrajpara787@gmail.com>
2026-05-11 10:56:30 +0530

    email serivece

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -11,19 +11,19 @@ const transporter = nodemailer.createTransport({
 exports.sendOTPByEmail  = async (email, otp) => {
   try {
      console.log("Sending email to:", email);
+      console.log("Sending otp to:", otp);
 
-     
-    await transporter.sendMail({
-      from: `"CartEase" <${process.env.EMAIL}>`,
-      to: email,
-      subject: "Your OTP Code",
-      html: `
-        <h2>OTP Verification</h2>
-        <p>Your OTP is:</p>
-        <h1>${otp}</h1>
-        <p>This OTP will expire in 5 minutes.</p>
-      `
-    });
+    // await transporter.sendMail({
+    //   from: `"CartEase" <${process.env.EMAIL}>`,
+    //   to: email,
+    //   subject: "Your OTP Code",
+    //   html: `
+    //     <h2>OTP Verification</h2>
+    //     <p>Your OTP is:</p>
+    //     <h1>${otp}</h1>
+    //     <p>This OTP will expire in 5 minutes.</p>
+    //   `
+    // });
 
     console.log("✅ Email sent successfully");
   } catch (error) {
